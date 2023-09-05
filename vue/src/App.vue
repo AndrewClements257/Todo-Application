40bfb33b2adaff38f00363243cee87bd371ba873
Andrew Clements <andrewclements257@gmail.com>
2023-09-04 20:31:27 -0400

    Experimented with the nav bar.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <!--
+   <!--
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     -->
@@ -16,5 +16,15 @@
   background-size: cover;
   /* font-family: Montserrat; */
   min-height: 100vh;
+  /* position: fixed; */
 }
+
+#nav {
+  height: 50px;
+  display: flex;
+  align-items: center;
+  margin: 0px 20px;
+  color: white;
+}
+
 </style>
